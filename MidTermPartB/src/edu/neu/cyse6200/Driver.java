455d591e6b9e9e28bd8b7945c499e2988a5d0fc5
amireddym <amireddy.m@northeastern.edu>
2021-11-07 23:22:02 -0500

    Output changed

--- MidTermPartB/src/edu/neu/cyse6200/Driver.java
+++ MidTermPartB/src/edu/neu/cyse6200/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		NortheasternU.demo();
-		
+	
 //		----------Displaying Intial Status-------------
 //		Employee [employeeWage=1230.5, Id=13, FirstName=daniel, LastName=peters, Age=34]
 //		Employee [employeeWage=1220.57, Id=10, FirstName=stalin, LastName=singh, Age=33]
@@ -22,7 +22,7 @@ public class Driver {
 //		Student [gpa=3.67, Id=2, FirstName=raja, LastName=singh, Age=21]
 //		Student [gpa=3.74, Id=1, FirstName=manoj, LastName=reddy, Age=24]
 //		Student [gpa=3.57, Id=3, FirstName=shashank, LastName=pragada, Age=23]
-//		--------After Employees Sorted By FirstName----------------		
+//		--------After Employees Sorted By FirstName----------------
 //		Employee [employeeWage=1230.5, Id=13, FirstName=daniel, LastName=peters, Age=34]
 //		Employee [employeeWage=1130.56, Id=12, FirstName=guna, LastName=shekar, Age=30]
 //		Employee [employeeWage=1220.57, Id=10, FirstName=stalin, LastName=singh, Age=33]
@@ -95,7 +95,7 @@ public class Driver {
 //		Student [gpa=3.57, Id=3, FirstName=shashank, LastName=pragada, Age=23]
 //		Student [gpa=3.67, Id=2, FirstName=raja, LastName=singh, Age=21]
 //		Student [gpa=3.74, Id=1, FirstName=manoj, LastName=reddy, Age=24]
-
+		
 	}
 
 }
